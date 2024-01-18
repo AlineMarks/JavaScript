cb3a215e704030b5961187235a2e4c323061072b
AlineMarks <113206932+AlineMarks@users.noreply.github.com>
2024-01-18 16:48:07 -0300

    modificações no arquivo

--- atividade01.17/atividade04.js
+++ atividade01.17/atividade04.js
@@ -1,14 +1,18 @@
 // Desenvolva uma função que determine se um número é primo ou não. Retorne true se for primo e false se não for.
 // divisivel por ele mesmo e por 1
 
-const propmt = require ('prompt-sync')()
+const propmt = require('prompt-sync')()
 
 const numero = Number(propmt('informe um numero: '))
 
-function numeroPrimo(umNumero){
-    if (umNumero%umNumero === 0 && umNumero%2 !== 0 && umNumero%3 !==0 && umNumero%5 !== 0 && umNumero%7 !== 0 ){
+function numeroPrimo(umNumero) {
+
+    if (umNumero % umNumero === 0 && umNumero % 2 !== 0 && umNumero % 3 !== 0 && umNumero % 5 !== 0 && umNumero % 7 !== 0) {
         return true
-    }else {
+    } else if (umNumero === 2 || umNumero === 3 || umNumero === 5 || umNumero === 7) {
+        return true
+    }
+    else {
         return false
     }
 }
